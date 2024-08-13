a8a883dd7e92e431058dfa00213da7f8a8662fc5
Daniel Hindrikes <daniel@hindrikes.se>
2024-08-13 10:53:43 +0200

    Add project files.

---
+++ FlyoutItemVisibleBug/AppShell.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace FlyoutItemVisibleBug
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+
+            BindingContext = new AppShellViewModel();
+        }
+    }
+}
---
+++ FlyoutItemVisibleBug/AppShellViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+
+namespace FlyoutItemVisibleBug
+{
+    public class AppShellViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public AppShellViewModel()
+        {
+        }
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+        private bool showItem;
+        public bool ShowItem
+        {
+            get => showItem;
+            set
+            {
+                showItem = value;
+                OnPropertyChanged(nameof(ShowItem));
+            }
+        }
+    }
+}
